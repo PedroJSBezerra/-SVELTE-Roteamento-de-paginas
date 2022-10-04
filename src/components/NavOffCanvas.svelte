c8be8af5eef36c48543e66a7711a0f02503844a0
PedroJSBezerra <52613930+PedroJSBezerra@users.noreply.github.com>
2022-10-03 22:28:42 -0300

    1 file modified

--- src/components/NavOffCanvas.svelte
+++ src/components/NavOffCanvas.svelte
@@ -1,5 +1,11 @@
 <script>
   import { location } from "svelte-spa-router"
+  window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
 </script>
 
 <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
