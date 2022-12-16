8ea48090758497cc705f6d79aede2f8fd94b5ada
PedroJSBezerra <52613930+PedroJSBezerra@users.noreply.github.com>
2022-12-16 16:20:18 -0300

    1 file modified

--- src/routes/PasswordGenerator.svelte
+++ src/routes/PasswordGenerator.svelte
@@ -34,19 +34,21 @@
 
       <div class="input">
         <label for="tamanho">
-          <p>Escolha o tamanho da senha em caracteres:</p>
-          <input id="tamanho" type="number" bind:value={length}>
+          <p><i class="bi bi-info"></i>Escolha o tamanho da senha em caracteres:</p>
         </label>
+        <input id="tamanho" type="number" bind:value={length}>
         <button on:click={() => generatePassword()}>Gerar Senha</button>
       </div>
 
       <div class="output">
         <p>Sua senha gerada é:</p>
         {#if password != ''}
-          <h3>{password}</h3>
-          <button class="copy" on:click={() => copy(password)}>
-            <i style="color:{copied?"green":""};" class="bi bi-clipboard{copied?"-check":""}"></i>
-          </button>
+          <div class="result">
+            <h3>{password}</h3>
+            <button class="copy" on:click={() => copy(password)}>
+              <i style="color:{copied?"green":""};" class="bi bi-clipboard{copied?"-check":""}"></i>
+            </button>
+          </div>
         {/if}    
       </div>
     
@@ -59,27 +61,41 @@
     box-shadow: 2px 1px 8px #999;
     border-radius: 4px;
     padding: 10px;
-    display: flex;
-    flex-direction: column;
+  }
+
+  .input {
+  }
+
+  .input button {
+    font-size: 1.2rem;
+  }
+
+  #tamanho {
+    width: 40px;
+    font-size: 1.4rem;
+  }
+
+  .output {
   }
 
   .result {
     display: flex;
+    justify-content: center;
     align-items: center;
   }
 
-  .copy {
+  button.copy {
     background: none;
     border: none;
     font-size: 2rem;
-    padding: 2px;
+    padding: 0;
+    margin: -1px;
+    border: 1px dashed rgb(0 0 0 / 0%);
+    margin-left: 10px;
   }
 
-  .copy:hover {
+  button.copy:hover {
     border: 1px dashed blue;
-  }
-
-  .bi-clipboard:hover {
     color: blue;
   }
 </style>
