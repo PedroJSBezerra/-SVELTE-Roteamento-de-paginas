efb622f42bfd05556056a3ef76f806aaa0b42a79
PedroJSBezerra <52613930+PedroJSBezerra@users.noreply.github.com>
2022-12-16 15:32:09 -0300

    2 files modified

--- src/routes/PasswordGenerator.svelte
+++ src/routes/PasswordGenerator.svelte
@@ -1,26 +1,29 @@
 <script>
-  import { onMount } from 'svelte';
 
   let password = ''
   let length = 8
+
+  let copied = false
 
   const generatePassword = () => {
     
     const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     
-    let retVal = "";
+    let retVal = ""
     
     for (let i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
+      retVal += charset.charAt(Math.floor(Math.random() * n))
     }
     
-    password = retVal;
-   
-    return retVal;
+    password = retVal
+    copied = false 
+    return retVal
   }
 
+
   let copy = (text) => {
-    navigator.clipboard.writeText(text);
+    navigator.clipboard.writeText(text)
+    copied = true
   }
 
 </script>
@@ -35,8 +38,21 @@
     <p>Sua senha gerada é:</p>
     {#if password != ''}
       <h3>{password}</h3>
-      <button on:click={() => copy(password)}>Copiar</button>
+      <button class="copy" on:click={() => copy(password)}>
+        <i style="color:{copied?"green":""};" class="bi bi-clipboard{copied?"-check":""}"></i>
+      </button>
     {/if}
   </div>
 </section>
 
+<style>
+  .copy {
+    border: none;
+    background: none;
+    font-size: 2rem;
+  }
+
+  .bi-clipboard:hover {
+    color: blue;
+  }
+</style>
