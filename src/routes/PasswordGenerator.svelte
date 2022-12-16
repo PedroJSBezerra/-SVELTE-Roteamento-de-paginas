748ad925de77b94dcfdbb45e86e25c7cb1d7c214
PedroJSBezerra <52613930+PedroJSBezerra@users.noreply.github.com>
2022-12-16 15:55:30 -0300

    1 file modified

--- src/routes/PasswordGenerator.svelte
+++ src/routes/PasswordGenerator.svelte
@@ -30,26 +30,53 @@
 
 <section class="section">
   <div class="container">
-    <label for="tamanho">
-      <p>Escolha o tamanho da senha em caracteres:</p>
-      <input id="tamanho" type="number" bind:value={length}>
-    </label>
-    <button on:click={() => generatePassword()}>Gerar Senha</button>
-    <p>Sua senha gerada é:</p>
-    {#if password != ''}
-      <h3>{password}</h3>
-      <button class="copy" on:click={() => copy(password)}>
-        <i style="color:{copied?"green":""};" class="bi bi-clipboard{copied?"-check":""}"></i>
-      </button>
-    {/if}
+    <div class="row">
+
+      <div class="input">
+        <label for="tamanho">
+          <p>Escolha o tamanho da senha em caracteres:</p>
+          <input id="tamanho" type="number" bind:value={length}>
+        </label>
+        <button on:click={() => generatePassword()}>Gerar Senha</button>
+      </div>
+
+      <div class="output">
+        <p>Sua senha gerada é:</p>
+        {#if password != ''}
+          <h3>{password}</h3>
+          <button class="copy" on:click={() => copy(password)}>
+            <i style="color:{copied?"green":""};" class="bi bi-clipboard{copied?"-check":""}"></i>
+          </button>
+        {/if}    
+      </div>
+    
+    </div>
   </div>
 </section>
 
 <style>
+  .row {
+    box-shadow: 2px 1px 8px #999;
+    border-radius: 4px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .result {
+    display: flex;
+    align-items: center;
+  }
+
   .copy {
-    border: none;
     background: none;
+    border: none;
     font-size: 2rem;
+    padding: 2px;
+  }
+
+  .copy:hover {
+    border: 1px dashed blue;
   }
 
   .bi-clipboard:hover {
